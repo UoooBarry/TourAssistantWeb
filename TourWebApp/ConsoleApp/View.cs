bcefe17cb2530e5aa013e74f6b65501d26dc5242
noobfromvn99 <37392152+noobfromvn99@users.noreply.github.com>
2020-04-10 21:09:57 +1000

    Add connection to database, Test connection

---
+++ TourWebApp/ConsoleApp/View.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourWebApp.ConsoleApp
+{
+    public class View
+    {
+        public void Display()
+        {
+            String output = "";
+            string connetionString = @"Server=s3673712.database.windows.net;Database=SEPM;uid=s3673712;pwd=Bach12345;MultipleActiveResultSets=true";
+            SqlConnection connect = new SqlConnection(connetionString);
+            connect.Open();
+            SqlCommand command = new SqlCommand("Select LoginID,UserID from dbo.Logins", connect);
+            SqlDataReader dr = command.ExecuteReader();
+            while (dr.Read()) {
+                
+                output = output + "LoginID: " + dr.GetValue(0) + " - UserID: " + dr.GetValue(1);
+            }
+            while (true)
+            {
+                Console.WriteLine(output);
+                Console.Write(
+ @"==============================================
+WELCOME TO Tour Management System
+Select the application that want to run
+==============================================
+1. Login
+2. Quit
+");
+                var selection = Console.ReadLine();
+                int number;
+                if (!Int32.TryParse(selection, out number))
+                {
+                    Console.WriteLine("Invalid intput");
+                    continue;
+                }
+                switch (number)
+                {
+                    case 1:
+                   
+                        break;
+                    case 2:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        Console.WriteLine("Press any key to go back . . .");
+                        Console.ReadKey();
+                        Console.WriteLine("");
+                        break;
+                }
+
+
+            }
+        }
+    }
+   
+}
--- TourWebApp/Program.cs
+++ TourWebApp/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TourWebApp.Data;
+using TourWebApp.ConsoleApp;
 
 namespace TourWebApp
 {
@@ -15,23 +16,23 @@ namespace TourWebApp
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    SeedItems.Initialize(services); //Insert seed items to database
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
-
-
-            host.Run();
+            //var host = CreateHostBuilder(args).Build();
+            //using (var scope = host.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+            //    try
+            //    {
+            //        SeedItems.Initialize(services); //Insert seed items to database
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var logger = services.GetRequiredService<ILogger<Program>>();
+            //        logger.LogError(ex, "An error occurred seeding the DB.");
+            //    }
+            //}
+            //host.Run();
+            View app = new View();
+            app.Display();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
