9df911085cf5ee771b5c2566f5836bc91f464291
UoooBarry <37783883+UoooBarry@users.noreply.github.com>
2020-04-11 20:57:28 +0800

    Added location set

---
+++ TourWebApp/Migrations/20200411125601_newLocationSet.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TourWebApp.Migrations
+{
+    public partial class newLocationSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LocationSetName",
+                table: "LocationSets",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationSetName",
+                table: "LocationSets");
+        }
+    }
+}
--- TourWebApp/Models/LocationSet.cs
+++ TourWebApp/Models/LocationSet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,10 @@ namespace TourWebApp.Models
     public class LocationSet
     {
         public int LocationSetID { get; set; }
+
+        [Required]
+        public string LocationSetName{get;set;}
+
         public virtual List<Location> Locations { get; set; }
 
         public virtual List<Tour> Tour { get; set; }
